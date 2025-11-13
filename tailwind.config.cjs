f448a4f89f58e15cbf2ef748864e9663e1fdabce
grontho69 <mahathirm880@gmail.com>
2025-11-13 23:19:20 +0600

    fifth commit

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,11 +2,11 @@
 module.exports = {
   content: [
     "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
 
